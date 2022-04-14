30637494b10ba6b73f7610cfd36b2ac6afce5d7f
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-14 01:35:59 +0100

    Add files

---
+++ include/CodeSmithy/Doxygen/Core.hpp
@@ -0,0 +1,13 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/codesmithyide/doxygen/blob/main/LICENSE.txt
+*/
+
+#ifndef _CODESMITHY_DOXYGEN_CORE_HPP_
+#define _CODESMITHY_DOXYGEN_CORE_HPP_
+
+#include "Core/DoxygenXMLOutput.hpp"
+#include "Core/linkoptions.hpp"
+
+#endif
