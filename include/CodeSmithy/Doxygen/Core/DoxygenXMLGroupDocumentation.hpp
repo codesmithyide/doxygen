b8289a76fd3304aeb183da3584b91442248eefaa
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-16 01:58:10 +0100

    Implement and test DoxygenXMLGroupDocumentation

--- include/CodeSmithy/Doxygen/Core/DoxygenXMLGroupDocumentation.hpp
+++ include/CodeSmithy/Doxygen/Core/DoxygenXMLGroupDocumentation.hpp
@@ -7,10 +7,30 @@
 #ifndef _CODESMITHY_DOXYGEN_CORE_DOXYGENXMLGROUPDOCUMENTATION_HPP_
 #define _CODESMITHY_DOXYGEN_CORE_DOXYGENXMLGROUPDOCUMENTATION_HPP_
 
+#include <boost/filesystem.hpp>
+#include <string>
+
 namespace CodeSmithy
 {
     class DoxygenXMLGroupDocumentation
     {
+    public:
+        struct MemberInfo
+        {
+            MemberInfo(std::string name, std::string refId);
+
+            std::string name;
+            std::string refId;
+        };
+
+        static DoxygenXMLGroupDocumentation FromFile(const boost::filesystem::path& path);
+
+        const std::string& name() const;
+        const std::vector<MemberInfo>& members() const;
+
+    private:
+        std::string m_name;
+        std::vector<MemberInfo> m_members;
     };
 }
 
